7e029cb051731d5c82c7aa70a79025a757b6e47a
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2022-09-28 14:47:33 -0300

    feat: create Register.dart

--- codigo/mobile/lib/view/Login.dart
+++ codigo/mobile/lib/view/Login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/view/Register.dart';
+import 'package:mobile/view/main.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -14,63 +16,107 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Colors.purpleAccent,
       body: Center(
-        child: Container(
-          color: Colors.deepPurple,
-          width: 400,
-          height: 200,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextFormField(
-                        textAlign: TextAlign.start,
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        enabled: true,
-                        decoration: const InputDecoration(
-                          labelText: "E-mail",
-                        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 32.0, top: 0.0, right: 32.0, bottom: 0.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                color: Colors.deepPurple,
+                width: 400,
+                height: 250,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              textAlign: TextAlign.start,
+                              controller: _emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              enabled: true,
+                              decoration: const InputDecoration(
+                                labelText: "E-mail",
+                                  labelStyle: const TextStyle(
+                                      color: Colors.white
+                                  )
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextFormField(
-                        textAlign: TextAlign.start,
-                        controller: _passwordController,
-                        keyboardType: TextInputType.emailAddress,
-                        enabled: true,
-                        decoration: const InputDecoration(
-                          labelText: "Senha"
-                        ),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              textAlign: TextAlign.start,
+                              controller: _passwordController,
+                              keyboardType: TextInputType.emailAddress,
+                              enabled: true,
+                              decoration: const InputDecoration(
+                                labelText: "Senha",
+                                labelStyle: const TextStyle(
+                                  color: Colors.white
+                                )
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // not implemented yet
-                  },
-                    style: const ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(Colors.purple),
-                      textStyle: MaterialStatePropertyAll(TextStyle(fontWeight: FontWeight.bold)),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 32.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const HomePage(title: 'Ficha do usuário logado')));
+                          },
+                          style: const ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll(Colors.purple),
+                            textStyle: MaterialStatePropertyAll(TextStyle(fontWeight: FontWeight.bold)),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.only(left: 32.0, top: 16.0, right: 32.0, bottom: 16.0),
+                            child: Text("ENTRAR"),
+                          )
                     ),
-                  child: const Padding(
-                    padding: EdgeInsets.only(left: 32.0, top: 16.0, right: 32.0, bottom: 16.0),
-                    child: Text("ENTRAR"),
-                  )
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: 400,
+                height: 100,
+                child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        const Padding(
+                          padding: EdgeInsets.only(right: 16.0),
+                          child: Text("Não tem uma conta?"),
+                        ),
+                        ElevatedButton(
+                          style: const ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll(Colors.deepPurple),
+                            textStyle: MaterialStatePropertyAll(TextStyle(fontWeight: FontWeight.bold)),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => RegisterPage()));
+                          },
+                          child: const Text("Cadastre-se"))
+                      ],
+                    )),
               )
-              ],
-            ),
+            ],
           ),
         ),
       ),
---
+++ codigo/mobile/lib/view/Register.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'Login.dart';
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purpleAccent,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 32.0, top: 0.0, right: 32.0, bottom: 0.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                color: Colors.deepPurple,
+                width: 400,
+                height: 450,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              textAlign: TextAlign.start,
+                              // controller: _emailController,
+                              keyboardType: TextInputType.text,
+                              enabled: true,
+                              decoration: const InputDecoration(
+                                  labelText: "Nome",
+                                  labelStyle: const TextStyle(
+                                      color: Colors.white
+                                  )
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              textAlign: TextAlign.start,
+                              // controller: _emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              enabled: true,
+                              decoration: const InputDecoration(
+                                  labelText: "E-mail",
+                                  labelStyle: const TextStyle(
+                                      color: Colors.white
+                                  )
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              textAlign: TextAlign.start,
+                              // controller: _emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              enabled: true,
+                              decoration: const InputDecoration(
+                                  labelText: "Confirmar e-mail",
+                                  labelStyle: const TextStyle(
+                                      color: Colors.white
+                                  )
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              textAlign: TextAlign.start,
+                              // controller: _passwordController,
+                              keyboardType: TextInputType.emailAddress,
+                              enabled: true,
+                              decoration: const InputDecoration(
+                                  labelText: "Senha",
+                                  labelStyle: const TextStyle(
+                                      color: Colors.white
+                                  )
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              textAlign: TextAlign.start,
+                              // controller: _passwordController,
+                              keyboardType: TextInputType.emailAddress,
+                              enabled: true,
+                              decoration: const InputDecoration(
+                                  labelText: "Confirmar senha",
+                                  labelStyle: const TextStyle(
+                                      color: Colors.white
+                                  )
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 32.0),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              // not implemented yet
+                            },
+                            style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(Colors.purple),
+                              textStyle: MaterialStatePropertyAll(TextStyle(fontWeight: FontWeight.bold)),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 32.0, top: 16.0, right: 32.0, bottom: 16.0),
+                              child: Text("CADASTRAR"),
+                            )
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: 400,
+                height: 100,
+                child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 16.0),
+                          child: const Text("Já possui uma conta?"),
+                        ),
+                        ElevatedButton(
+                            style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(Colors.deepPurple),
+                              textStyle: MaterialStatePropertyAll(TextStyle(fontWeight: FontWeight.bold)),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => LoginPage()));
+                            },
+                            child: const Text("Entrar"))
+                      ],
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
