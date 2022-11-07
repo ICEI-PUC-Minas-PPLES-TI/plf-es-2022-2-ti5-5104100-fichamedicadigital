56201b5633208f815ed2882e7294f41cb7acf1ec
Rodolfo Rocha Rodrigues <1299252@sga.pucminas.br>
2022-11-07 20:56:13 -0300

    adicionando loading login

--- codigo/frontend/src/components/views/DashBoard/DashBoard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Main from '../../template/Main/Main'
-
-export default props => 
-    <Main icon="pie-chart" title="Dashboard" 
-        subtitle="Dashboard Admin">
-        <p className='mb-0'>Teste</p>
-    </Main>
