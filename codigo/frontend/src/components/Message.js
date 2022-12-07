18eff34b6e84aa0abae7ebdd1be516b15cc37d56
Rodolfo Rocha Rodrigues <rodolforrodrigues14@gmail.com>
2022-12-07 10:10:27 -0300

    atualizando mensageria

--- codigo/frontend/src/components/Message.js
+++ codigo/frontend/src/components/Message.js
@@ -1,39 +1,39 @@
 import React from 'react'
+import './Message.css'
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { mensageria,mensageriaDelete } from '../slices/consultSlice'
+import { mensageria, mensageriaDelete } from '../slices/consultSlice'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Message = () => {
 
     const dispatch = useDispatch()
+    
     const user = useSelector((state) => state.auth.user)
     const notificacoes = useSelector((state) => state.consult.notificacoes)
-    console.log(notificacoes)
+    const customId = "custom-id-yes";
+
     useEffect(() => {
         dispatch(mensageria(user.id))
-    },[])
+    }, [])
 
-    const handleDelete = () => {
-        dispatch(mensageriaDelete(notificacoes.idUsuario))
-        setTimeout(function() {
-            window.location.reload(1);
-          }, 1000);
-    }
-
-  return (
-    <>
-    {notificacoes && notificacoes.id != null && 
-        <div class="row alert alert-warning w-25 mt-2 ms-2 border-bottom" role="alert">
-            <div className='col-9'>
-            {notificacoes.descricao}
-            </div>
-            <div className='col-3'>
-                <button className='btn' onClick={handleDelete}>X</button>
-            </div>
-        </div>
-    }
-    </>
-  )
+    const notify = (notificacao) => {
+        toast(`${notificacao}`, {
+          toastId: customId
+        });
+      }
+    return (
+        <>
+            {notificacoes && notificacoes.id != null && (
+                <div>
+                    {notify(notificacoes.descricao)}
+                    <ToastContainer/>
+                </div>
+            )
+            }
+        </>
+    )
 }
 
 export default Message
